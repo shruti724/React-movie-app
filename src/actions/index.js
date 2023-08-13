aaa379e3748df07bf8fc7295ab7dea7509df467b
S.R JHA <shrutiranjanjha274@gmail.com>
2023-08-13 10:18:23 +0530

    Adding actions

---
+++ src/actions/index.js
@@ -0,0 +1,15 @@
+// Actions are just a javascript object.
+// through which we express an intent to modify the state.
+
+// Actions act as a breadcrupms for the user-actions.
+
+// ACTIONS
+
+{
+    type: 'ADD_MOVIES',
+    movies: [m1, m2, m3]
+}
+
+{
+    type: 'DELETE_MOVIES'
+}
