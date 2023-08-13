a2662042b90ae36d391ff9329b5c353aa14bee75
S.R JHA <shrutiranjanjha274@gmail.com>
2023-08-13 12:05:23 +0530

    Creating store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+// Package imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from "redux";
+
+// File imports
 import './index.css';
 import App from './components/App';
+import movies from './reducers';
+
+const store = createStore(movies);
+console.log('store',store);
+// getState function of store
+console.log('STATE', store.getState());
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,15 @@
+// Here state is the default state of the store.
+
 export default function movies (state = [], action) {
     if (action.type === 'ADD_MOVIES'){
         return action.movies;
     }
     return state;
 }
+
+// internally movies reducer is called like this:
+
+// movies(undefined, {})
+// o/p: STATE: []
+
+// Redux is calling out movies reducers like this and if it is undefine, our state will take the initial emty array getValue.
