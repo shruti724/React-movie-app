ccc5b6525730bcd99916ef5e5f4ed226e9e54ff2
S.R JHA <shrutiranjanjha274@gmail.com>
2023-08-13 13:20:27 +0530

    dispatch function

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ import movies from './reducers';
 const store = createStore(movies);
 console.log('store',store);
 // getState function of store
-console.log('STATE', store.getState());
+// BEFORE STATE is the state before we send the action
+console.log('BEFORE STATE', store.getState());
+
+// dispatch() function of store
+store.dispatch({
+  type: 'ADD_MOVIES',
+  movies: [{name: 'Superman'}]
+}); 
+
+// AFTER STATE is after we send the action
+console.log('AFTER STATE', store.getState());
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
